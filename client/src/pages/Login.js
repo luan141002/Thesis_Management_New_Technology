ddd8c2e06d9ec691187d6e51d22adb8e7423620c
Quoc Thoai <thoaifamily83@gmail.com>
2023-12-16 16:20:24 +0700

    change text in login form

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -67,7 +67,7 @@ export default function Login() {
               <LockOutlinedIcon />
             </Avatar>
             <Typography component="h1" variant="h5" >
-              Sign in
+              Login
             </Typography>
             <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
               <TextField
