89bbab6fd20ee8976f81902ffba3754391550d23
khoa36isefiw <huynhdangkhoa961617@gmail.com>
2023-12-22 18:03:54 +0700

    change icons

--- client/src/layouts/AdminLayout/Sidebar/Sidebar.js
+++ client/src/layouts/AdminLayout/Sidebar/Sidebar.js
@@ -18,64 +18,74 @@ import { Link } from "react-router-dom";
 import { styled } from "@mui/material";
 import authService from "../../../services/authServices";
 import { useNavigate } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import accountsSlices from "../../../redux/accountsSlice";
-
+import PsychologyAltIcon from '@mui/icons-material/PsychologyAlt';
+import ScheduleSendIcon from '@mui/icons-material/ScheduleSend';
+import PersonIcon from '@mui/icons-material/Person';
+import SettingsEthernetIcon from '@mui/icons-material/SettingsEthernet';
+import TerminalIcon from '@mui/icons-material/Terminal';
+import LogoutIcon from '@mui/icons-material/Logout';
+import AppRegistrationIcon from '@mui/icons-material/AppRegistration';
+import PortraitIcon from '@mui/icons-material/Portrait';
+import TopicIcon from '@mui/icons-material/Topic';
+import ApprovalIcon from '@mui/icons-material/Approval';
+import ApartmentIcon from '@mui/icons-material/Apartment';
 const StyledListItemIcon = styled(ListItemIcon)({
   minWidth: "40px",
 });
 function Sidebar() {
   const navigate = useNavigate();
-  const dispatch = useDispatch();
   const [selectedIndex, setSelectedIndex] = React.useState(0);
   const [settings, setSettings] = useState();
   const handleListItemClick = (index) => {
     setSelectedIndex(index);
   };
-
-  const account = useSelector((state) => state.account);
+  const account = JSON.parse(localStorage.getItem("account"));
 
   const role = account.type;
   useEffect(() => {
     if (role === "student")
       setSettings([
-        { title: "Profile", link: "/student/profile" },
-        { title: "Register Thesis", link: "/student/assign-thesis" },
-        { title: "Manage Thesis", link: "/student/manage-thesis" },
-        { title: "Logout", link: "/logout" },
+        { title: "Profile", link: "/student/profile", icon2: PortraitIcon },
+        { title: "Register Thesis", link: "/student/assign-thesis", icon2: AppRegistrationIcon },
+        { title: "Manage Thesis", link: "/student/manage-thesis", icon2: TopicIcon },
+        { title: "Logout", link: "/logout", icon2: LogoutIcon },
       ]);
     else if (role === "faculty") {
       if (account.isHeadDep)
         setSettings([
-          { title: "Profile", link: "/faculty/profile" },
-          { title: "Register Thesis", link: "/faculty/register-thesis" },
-          { title: "Manage Thesis", link: "/head-lecturer/manage-thesis" },
-          { title: "Approval Thesis", link: "/head-lecturer/pending-thesis" },
+          { title: "Profile", link: "/faculty/profile" ,icon2: PortraitIcon},
+          { title: "Register Thesis", link: "/faculty/register-thesis" ,icon2: AppRegistrationIcon },
+          { title: "Manage Thesis", link: "/head-lecturer/manage-thesis",icon2: TopicIcon },
+          { title: "Approval Thesis", link: "/head-lecturer/pending-thesis", icon2: ApprovalIcon },
           {
             title: "Distribute Faculty",
             link: "/head-lecturer/approved-thesis",
+            icon2: ApartmentIcon,
           },
-          { title: "Logout", link: "/logout" },
+          { title: "Logout", link: "/logout", icon2: LogoutIcon },
         ]);
       else
         setSettings([
-          { title: "Profile", link: "/faculty/profile" },
-          { title: "Register Thesis", link: "/faculty/register-thesis" },
-          { title: "Manage Thesis", link: "/faculty/manage-thesis" },
-          { title: "Logout", link: "/logout" },
+          { title: "Profile", link: "/faculty/profile" ,icon2: PortraitIcon},
+          { title: "Register Thesis", link: "/faculty/register-thesis", icon2: AppRegistrationIcon },
+          { title: "Manage Thesis", link: "/faculty/manage-thesis",icon2: TopicIcon },
+          { title: "Logout", link: "/logout", icon2: LogoutIcon },
         ]);
     } else if (role === "admin") {
       setSettings([
-        { title: "Dashboard", link: "/dashboard" },
-        { title: "Manage Student", link: "/manage-student" },
-        { title: "Manage Lecturer", link: "/manage-faculty" },
-        { title: "Manage Schedule", link: "/manage-schedule" },
-        { title: "Manage Thesis", link: "/manage-thesis" },
-        { title: "Manage Term", link: "/manage-term" },
-        { title: "Logout", link: "/logout" },
+        { title: "Dashboard", link: "/dashboard", icon2:EqualizerIcon},
+        { title: "Manage Student", link: "/manage-student", icon2:PersonIcon},
+        { title: "Manage Lecturer", link: "/manage-faculty",  icon2: PsychologyAltIcon},
+        { title: "Manage Schedule", link: "/manage-schedule", icon2: ScheduleSendIcon  },
+        { title: "Manage Thesis", link: "/manage-thesis", icon2: TopicIcon},
+        { title: "Manage Term", link: "/manage-term", icon2:TerminalIcon },
+        { title: "Logout", link: "/logout" , icon2: LogoutIcon},
+        
       ]);
     }
   }, []);
+
+  
 
   const drawer = (
     <Box height="100%" sx={{ backgroundColor: "rgb(28,37,54)" }}>
@@ -87,12 +97,12 @@ function Sidebar() {
           component="a"
           href="/"
           sx={{
-            mr: 2,
+            // mr: 2,
             display: { xs: "none", md: "flex" },
-            fontSize: "18px",
+            fontSize: "20px",
             fontFamily: "monospace",
             fontWeight: 700,
-            letterSpacing: ".3rem",
+            letterSpacing: ".25rem",
             color: "#fff",
             textDecoration: "none",
           }}
@@ -103,25 +113,25 @@ function Sidebar() {
       <Box sx={{ overflow: "auto" }}>
         <List sx={{ ml: "8px" }}>
           {settings?.map((setting, index) => {
+            const IconComponent = setting.icon2 !== "EqualizerIcon" ? setting.icon2 : EqualizerIcon;
             console.log(setting);
             return (
-              <Link
-                Link
-                to={`${setting.link !== "/logout" ? setting.link : "/login"}`}
-              >
+              <Link Link to={`${setting.link}`}>
                 <ListItem disablePadding>
                   <ListItemButton
                     selected={selectedIndex === 0}
                     onClick={async () => {
                       handleListItemClick(index);
                       if (setting.title === "Logout") {
-                        dispatch(accountsSlices.actions.deleteAccount());
+                        await authService.logout(account);
+                        localStorage.removeItem("account");
                         navigate("/login");
                       }
                     }}
                   >
                     <StyledListItemIcon sx={{ minWidth: "40px" }}>
-                      <EqualizerIcon fontSize="medium" sx={{ color: "#fff" }} />
+                      
+                      <IconComponent fontSize="medium" sx={{ color: "#fff" }} />
                     </StyledListItemIcon>
                     <ListItemText
                       primary={`${setting.title}`}
@@ -155,102 +165,3 @@ function Sidebar() {
 }
 
 export default Sidebar;
-
-{
-  /* <Link to={"/manage-student"}>
-            <ListItem disablePadding>
-              <ListItemButton
-                selected={selectedIndex === 1}
-                onClick={() => handleListItemClick(1)}
-              >
-                <StyledListItemIcon>
-                  <PeopleIcon fontSize="medium" sx={{ color: "#fff" }} />
-                </StyledListItemIcon>
-                <ListItemText
-                  primary="Student"
-                  primaryTypographyProps={{ fontSize: "16px", color: "#fff" }}
-                />
-              </ListItemButton>
-            </ListItem>
-          </Link>
-          <Link to={"/manage-faculty"}>
-            <ListItem disablePadding>
-              <ListItemButton
-                selected={selectedIndex === 2}
-                onClick={() => handleListItemClick(2)}
-              >
-                <StyledListItemIcon>
-                  <PeopleIcon fontSize="medium" sx={{ color: "#fff" }} />
-                </StyledListItemIcon>
-                <ListItemText
-                  primary="Faculty"
-                  primaryTypographyProps={{ fontSize: "16px", color: "#fff" }}
-                />
-              </ListItemButton>
-            </ListItem>
-          </Link>
-          <Link to={"/manage-schedule"}>
-            <ListItem disablePadding>
-              <ListItemButton
-                selected={selectedIndex === 3}
-                onClick={() => handleListItemClick(3)}
-              >
-                <StyledListItemIcon>
-                  <ReceiptIcon fontSize="medium" sx={{ color: "#fff" }} />
-                </StyledListItemIcon>
-                <ListItemText
-                  primary="Schedule"
-                  primaryTypographyProps={{ fontSize: "16px", color: "#fff" }}
-                />
-              </ListItemButton>
-            </ListItem>
-          </Link>
-          <Link to={"/manage-thesis"}>
-            <ListItem disablePadding>
-              <ListItemButton
-                selected={selectedIndex === 4}
-                onClick={() => handleListItemClick(4)}
-              >
-                <StyledListItemIcon>
-                  <ReceiptIcon fontSize="medium" sx={{ color: "#fff" }} />
-                </StyledListItemIcon>
-                <ListItemText
-                  primary="Thesis"
-                  primaryTypographyProps={{ fontSize: "16px", color: "#fff" }}
-                />
-              </ListItemButton>
-            </ListItem>
-          </Link>
-          <Link to={"/manage-term"}>
-            <ListItem disablePadding>
-              <ListItemButton
-                selected={selectedIndex === 5}
-                onClick={() => handleListItemClick(5)}
-              >
-                <StyledListItemIcon>
-                  <ReceiptIcon fontSize="medium" sx={{ color: "#fff" }} />
-                </StyledListItemIcon>
-                <ListItemText
-                  primary="Term"
-                  primaryTypographyProps={{ fontSize: "16px", color: "#fff" }}
-                />
-              </ListItemButton>
-            </ListItem>
-          </Link>
-          <Link to={"/manage-major"}>
-            <ListItem disablePadding>
-              <ListItemButton
-                selected={selectedIndex === 6}
-                onClick={() => handleListItemClick(6)}
-              >
-                <StyledListItemIcon>
-                  <ReceiptIcon fontSize="medium" sx={{ color: "#fff" }} />
-                </StyledListItemIcon>
-                <ListItemText
-                  primary="Major"
-                  primaryTypographyProps={{ fontSize: "16px", color: "#fff" }}
-                />
-              </ListItemButton>
-            </ListItem>
-          </Link> */
-}
